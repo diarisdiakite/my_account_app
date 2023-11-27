2636a4b2bf767d488b48304a35b1e421828f0e1e
diarisdiakite <diarisdiakite@gmail.com>
2023-11-27 09:22:14 +0100

    Remove bootstrap css files. Clear cache. Add 'config.public_file_server.enabled = true'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Added on 27/11/2023 _____ To comment if any issue
+  config.public_file_server.enabled = true
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
